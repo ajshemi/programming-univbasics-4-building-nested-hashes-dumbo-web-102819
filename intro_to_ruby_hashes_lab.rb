09318119cfe189125e5e7ed04dfeb8fac004b1e2
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-18 03:40:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,9 @@ def monopoly_with_second_tier()
   # complexity.
 end
 
-def monopoly_with_third_tier
+def monopoly_with_third_tier()
+  new_hash=monopoly_with_second_tier()
+  new_hash={:railroads}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
